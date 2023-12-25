e038548c2917933e8236fc9c1292c1c1aa2c0c69
Paul <stantonp7@students.rowan.edu>
2023-12-25 11:47:34 -0500

    style navbar and add some demo items.

--- nextjs-demo/src/components/appNavbar.tsx
+++ nextjs-demo/src/components/appNavbar.tsx
@@ -1,9 +1,10 @@
 // app/components/ThemeSwitcher.tsx
 "use client";
 
-import { Navbar } from "@nextui-org/react";
+import { Button, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/react";
 import {useTheme} from "next-themes";
 import { useEffect, useState } from "react";
+import { AppLogo } from "./appLogo";
 
 export function AppNavbar() {
   const [mounted, setMounted] = useState(false)
@@ -14,12 +15,30 @@ export function AppNavbar() {
   }, [])
 
   return (
-    <Navbar shouldHideOnScroll>
-      {mounted && (
-        <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
-          Switch Theme
-        </button>
-      )}
+    <Navbar shouldHideOnScroll isBordered={true} maxWidth="full">
+      <NavbarContent justify="start">
+        <NavbarBrand>
+          <AppLogo/>
+          <p>Brand</p>
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        {mounted && (
+          <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+            Switch Theme
+          </button>
+        )}
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
     </Navbar>
   )
 };
